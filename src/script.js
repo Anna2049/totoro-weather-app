b40987ead3803f0c5643b5f089da01a090bc7d9b
Anna2049 <85588122+Anna2049@users.noreply.github.com>
2021-07-13 01:43:01 +0200

    initial

---
+++ src/script.js
@@ -0,0 +1,107 @@
+function showCurrentDateAndTime() {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let months = [
+    "Jan",
+    "Feb",
+    "March",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let now = new Date();
+  let month = months[now.getMonth()];
+  let date = now.getDate();
+  let day = days[now.getDay()];
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let time = `${hours}:${minutes}`;
+  currentDateAndTime.innerHTML = `${month} ${date}, ${day}, ${time}`;
+}
+
+function enableGPS() {
+  navigator.geolocation.getCurrentPosition(createApiRouteByGPS);
+}
+
+function createApiRouteByGPS(position) {
+  let units = "metric";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function createApiRouteByCityName(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function showWeather(response) {
+  currentTemperature.innerHTML = Math.round(response.data.main.temp);
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = `${response.data.name} (${response.data.sys.country})`;
+  let currentWeatherDescription = document.querySelector(
+    "#current-description"
+  );
+  currentWeatherDescription.innerHTML = response.data.weather[0].description;
+}
+
+/*other stuff to be included:
+  "country": "US",
+  description": "clear sky",
+  "feels_like": 281.86,
+    "temp_min": 280.37,
+    "temp_max": 284.26,
+    "pressure": 1023,
+    "humidity": 100
+    "sunrise": 1560343627,
+    "sunset": 1560396563
+ */
+
+function changeUOM(event) {
+  event.preventDefault();
+  let currentTempResponse = document.getElementById(
+    "current-temperature"
+  ).textContent;
+  if (currentTempResponse === "") {
+  } else {
+    currentTempResponse = parseInt(currentTempResponse);
+    if (event.target.id === "fahrenheit") {
+      currentTemperature.innerHTML = Math.round(currentTempResponse * 1.8 + 32);
+      fahrenheitUOM.removeEventListener("click", changeUOM);
+      celsiusUOM.addEventListener("click", changeUOM);
+    } else {
+      currentTemperature.innerHTML = Math.round(
+        (currentTempResponse - 32) / 1.8
+      );
+      celsiusUOM.removeEventListener("click", changeUOM);
+      fahrenheitUOM.addEventListener("click", changeUOM);
+    }
+  }
+}
+
+let currentDateAndTime = document.querySelector("#date-and-time");
+showCurrentDateAndTime(currentDateAndTime);
+
+let apiKey = "13e9496ba2a5643119025f905a5f6396";
+let currentTemperature = document.querySelector("#current-temperature");
+
+let buttonGps = document.querySelector("#button-location-gps");
+buttonGps.addEventListener("click", enableGPS);
+let inputFormCity = document.querySelector("#search-city-form");
+inputFormCity.addEventListener("submit", createApiRouteByCityName);
+
+let fahrenheitUOM = document.querySelector("#fahrenheit");
+let celsiusUOM = document.querySelector("#celsius");
+fahrenheitUOM.addEventListener("click", changeUOM);
